1453998bd5fb5543488d9e946e6bd9d788fa8557
jasongoose <ssjk7789@gmail.com>
2024-06-28 23:16:32 +0900

    최소한의 스타일로 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <div className="bg-green-500 h-screen"></div>;
-}
+import Accordian, { AccordianProps } from "./components/Accordian";
 
-export default App;
+const items: AccordianProps["items"] = [
+  {
+    title: "What is Github and how does it work?",
+    content:
+      "GitHub is the home for all developers—a platform where you can share code, contribute to open source projects, or even automate your workflow with tools like GitHub Actions and Packages. If you’re just getting started with GitHub, you may know us best as a place for version control and collaboration.",
+  },
+  {
+    title: "How do I see GitHub's availability?",
+    content: "Check our real-time status report",
+  },
+  {
+    title: "Why is GitHub so popular?",
+    content:
+      "GitHub is built by developers for developers, and we’re proud to be home to the world’s largest open source community. With 50 million developers and millions more open source projects, GitHub has become the go-to place to collaborate and build software together.",
+  },
+];
+
+export default function App() {
+  return (
+    <main className="w-[816px] mx-auto">
+      <Accordian {...{ items }} />
+    </main>
+  );
+}
